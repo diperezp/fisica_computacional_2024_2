f7687350ccf4d5ee8da694d01b7178d47c4a9379
diperezp <diperezp@unal.edu.co>
2025-02-10 22:09:40 -0500

    Refactor numerical integration functions to include open trapezoidal and Simpson's rules; update integration limits and iterations

--- integracion_numerica/integracion_numerica.f90
+++ integracion_numerica/integracion_numerica.f90
@@ -3,23 +3,26 @@ program integracion_numerica
     real::PI=3.14159265
     real::a                        !limite inferior del intervalo de integracion
     real::b            !limite superior del intervalo de integracion
-    integer::n=100                    !numero de interaciones
+    integer::n=1000                   !numero de interaciones
     real::y
     real::integral_trapezoidal
+    real::integral_trapezoidal_abierta
+    real::integral_simpson_abierto
     real::integral_simpson
     real::integral
 
-    a=PI/36
-    b=0
+    a=0
+    b=PI/36
 
-    integral=EXP(1.0)-1
+    integral=2*PI*SQRT(0.5/9.8)/4
+    write(*,*)"Periodo",integral
 
-    y=integral_trapezoidal(a,b,n)
+    y=integral_trapezoidal_abierta(a,b,n)*SQRT(0.5/(2*9.8))
     write(*,*)"Regla trapezoidal",y
     write(*,*)"Error trapezoidal",(abs(integral-y)/integral)*100
 
 
-    y=integral_simpson(a,b,n)
+    y=integral_simpson_abierto(a,b,n)*SQRT(0.5/(2*9.8))
     write(*,*)"Regla Simpson",y
     write(*,*)"Error simpson",(abs(integral-y)/integral)*100
 
@@ -66,7 +69,6 @@ real function integral_simpson(a,b,n)
     !integramos numericamente la expresion
     suma=(f(a)+f(b))*(h/3)
     do i=1,n-1
-    print*,suma
         if (mod(i, 2) == 0) then
             suma = suma + 2 * f(a + i * h)*(h/3)
         else
@@ -75,5 +77,49 @@ real function integral_simpson(a,b,n)
     end do
     suma=suma
     integral_simpson=suma
+
+end function
+
+
+real function integral_trapezoidal_abierta(a,b,n)
+    real::a             !limite inferior del intervalo de integracion
+    real::b             !limite superior del intervalo de integracion
+    integer::n          !numero de subrectangulos
+    real::h             !paso de integracion
+    real::suma          !suma
+
+    !asignamos el valor del paso de integracion
+    h=(b-a)/n
+
+    !integramos numericamente la expresion
+    suma=(f(a))*(h/2)
+    do i=1,n-1
+        suma=suma+2*f(a+i*h)*(h/2)
+    end do
+    suma=suma
+    integral_trapezoidal_abierta=suma
+end function
+
+real function integral_simpson_abierto(a,b,n)
+    real::a             !limite inferior del intervalo de integracion
+    real::b             !limite superior del intervalo de integracion
+    integer::n          !numero de subrectangulos
+    real::h             !paso de integracion
+    real::suma          !suma
+
+    !asignamos el valor del paso de integracion
+    h=(b-a)/n
+
+    !integramos numericamente la expresion
+    suma=(f(a))*(h/3)
+    do i=1,n-1
+        if (mod(i, 2) == 0) then
+            suma = suma + 2 * f(a + i * h)*(h/3)
+        else
+            suma = suma + 4 * f(a + i * h)*(h/3)
+        end if
+    end do
+    suma=suma
+    integral_simpson_abierto=suma
 
 end function
